7bc640ebc9728a105803396458160c428856cedb
buck <me@bsavage.xyz>
2021-12-27 23:14:56 +0530

    added streaming upload rpc

--- src/service.ts
+++ src/service.ts
@@ -1,11 +1,12 @@
-import { ServerWritableStream, status } from "grpc";
-import { IFileStreamingServiceServer } from "../proto/streamer_grpc_pb";
-import { FileRequest, Chunk } from "../proto/streamer_pb";
+import { handleClientStreamingCall, sendUnaryData, ServerReadableStream, ServerWritableStream, status } from "grpc";
+import { FileStreamingServiceService, IFileStreamingServiceServer } from "../proto/streamer_grpc_pb";
+import { FileDescriptor, Chunk } from "../proto/streamer_pb";
 import * as fs from "fs";
+import * as stream from "stream";
 
 export class DefaultFileStreamingService implements IFileStreamingServiceServer {
 
-    read(call: ServerWritableStream<FileRequest, Chunk>) {
+    read(call: ServerWritableStream<FileDescriptor, Chunk>) {
 
         const path = call.request.getPath();
         const stream = fs.createReadStream(path);
@@ -46,5 +47,44 @@ export class DefaultFileStreamingService implements IFileStreamingServiceServer 
             console.error(`streaming call with peer ${call.getPeer()} ran into an error: ${err}`);
             stream.destroy(err);
         });
+    }
+
+    write(call: ServerReadableStream<Chunk>, callback: sendUnaryData<FileDescriptor>) {
+        const md = call.metadata;
+        const pathMd = md.get("path");
+        if (pathMd.length !== 1 || pathMd[0] === "") {
+            return callback({ ...new Error("path is required"), code: status.INVALID_ARGUMENT }, null);
+        }
+
+        const path = pathMd[0].toString();
+        console.debug(`preparing to write file at ${path}`);
+
+
+        const transform = new stream.PassThrough();
+        this.writeToFile(path, transform);
+
+        call.on("data", (data: Chunk) => {
+            call.pause();
+            transform.write(data.getChunk_asU8(), err => {
+                if (err) {
+                    console.error("cannot write data to writable stream", err);
+                    return;
+                }
+                call.resume();
+            });
+        });
+
+        call.on("end", () => {
+            transform.destroy();
+
+            const fd = new FileDescriptor();
+            fd.setPath(path);
+            callback(null, fd);
+        });
+    }
+
+    private writeToFile(path: string, data: stream.Readable) {
+        const writeStream = fs.createWriteStream(path);
+        data.pipe(writeStream);
     }
 }
